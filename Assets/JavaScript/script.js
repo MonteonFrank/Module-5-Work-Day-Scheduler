a17e0008df219e1ea6a2115cefa5d18a8c145d1c
Frank Monteon <frank.monteon@gmail.com>
2023-01-08 21:48:28 -0600

    Deploy the time block configuration and the time display

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -1,15 +1,56 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+//Global variable
+var btn = [];
 
-
+//Runs the code once the page is fully loaded
 $(document).ready(function () {
 
-    // TODO: Add code to display the current date in the header of the page.
+    //Displays the date on the Current Day ID in the month, day and year format
     var today = dayjs();
     $('#currentDay').text(today.format('MMM D, YYYY'));
     
-    
+    //Change the date format to obtain the hours in 24H format
+    var militaryhour = today.format("HH");
+    console.log(militaryhour);
+
+
+    //Initiate a for loop to compare the time and for the class to dinalically chate to past, present or future
+    for (var i = 0 ; i<19 ; i++){
+      //if the 24 hour fomrat matches the time block, then it changes the class to the present format      
+      if (i == militaryhour){
+        $("#hour-"+i).attr("class", "row time-block present");
+      }
+      
+      //if the 24 hour format is bigger than the time block, then it changes the class to the past format
+      if (i<militaryhour){
+        $("#hour-"+i).attr("class", "row time-block past");
+      }
+
+      //if the 24 hour format is smaller that the time block, then it changes the class to the future format
+      if(i>militaryhour){
+        $("#hour-"+i).attr("class", "row time-block future");
+      }
+}
+
+
+//Obtain the buttons and check if there is something in local storage
+
+
+for (var i = 8 ; i<19 ; i++){
+  btn[i] = $("#hour-"+i).children("button").click(function(){
+    alert("button " + this.btn[i] + " was clicked", position());
+      function positon(){
+        return this.btn[i];
+      }
+  }) 
+  console.log(btn[i]);
+}
+
+for (var i = 8 ; i<19 ; i++){
+  
+
+}
+
+
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
@@ -17,16 +58,11 @@ $(document).ready(function () {
     // time-block containing the button that was clicked? How might the id be
     // useful when saving the description in local storage?
     //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
+
     //
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
     //
-
   });
   
