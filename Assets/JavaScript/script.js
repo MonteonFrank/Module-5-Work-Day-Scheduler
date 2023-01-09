231ee3970c26b587b9b290bbd93708e7c6b6a68a
Frank Monteon <frank.monteon@gmail.com>
2023-01-09 12:39:33 -0600

    Application now working with setting and retrieving data from local storage. Code optimizing will be next

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -32,11 +32,72 @@ $(document).ready(function () {
 }
 
 
-//Obtain the buttons and check if there is something in local storage
+
+//Retrieve stored information from local storage and show it
+if (localStorage.getItem("#hour-8") != null){
+  var retrieveinfo8 = JSON.parse(localStorage.getItem("#hour-8"));
+  $("#hour-8").children("textarea").text(retrieveinfo8);
+  console.log(retrieveinfo8);
+}
+if (localStorage.getItem("#hour-9") != null){
+  var retrieveinfo9 = JSON.parse(localStorage.getItem("#hour-9"));
+  $("#hour-9").children("textarea").text(retrieveinfo9);
+  console.log(retrieveinfo8);
+}
+
+if (localStorage.getItem("#hour-10") != null){
+  var retrieveinfo10 = JSON.parse(localStorage.getItem("#hour-10"));
+  $("#hour-10").children("textarea").text(retrieveinfo10);
+  console.log(retrieveinfo10);
+}
+if (localStorage.getItem("#hour-11") != null){
+  var retrieveinfo11 = JSON.parse(localStorage.getItem("#hour-11"));
+  $("#hour-11").children("textarea").text(retrieveinfo11);
+  console.log(retrieveinfo10);
+}
+if (localStorage.getItem("#hour-12") != null){
+  var retrieveinfo12 = JSON.parse(localStorage.getItem("#hour-12"));
+  $("#hour-12").children("textarea").text(retrieveinfo12);
+  console.log(retrieveinfo12);
+}
+if (localStorage.getItem("#hour-13") != null){
+  var retrieveinfo13 = JSON.parse(localStorage.getItem("#hour-13"));
+  $("#hour-13").children("textarea").text(retrieveinfo13);
+  console.log(retrieveinfo13);
+}
+if (localStorage.getItem("#hour-14") != null){
+  var retrieveinfo14 = JSON.parse(localStorage.getItem("#hour-14"));
+  $("#hour-14").children("textarea").text(retrieveinfo14);
+  console.log(retrieveinfo14);
+}
+if (localStorage.getItem("#hour-15") != null){
+  var retrieveinfo15 = JSON.parse(localStorage.getItem("#hour-15"));
+  $("#hour-15").children("textarea").text(retrieveinfo15);
+  console.log(retrieveinfo15);
+}
+if (localStorage.getItem("#hour-16") != null){
+  var retrieveinfo16 = JSON.parse(localStorage.getItem("#hour-16"));
+  $("#hour-16").children("textarea").text(retrieveinfo16);
+  console.log(retrieveinfo16);
+}
+if (localStorage.getItem("#hour-17") != null){
+  var retrieveinfo17 = JSON.parse(localStorage.getItem("#hour-17"));
+  $("#hour-17").children("textarea").text(retrieveinfo17);
+  console.log(retrieveinfo17);
+}
+if (localStorage.getItem("#hour-18") != null){
+  var retrieveinfo18 = JSON.parse(localStorage.getItem("#hour-18"));
+  $("#hour-18").children("textarea").text(retrieveinfo18);
+  console.log(retrieveinfo18);
+}
 
 
+
+//For loop to avoid big code
+/*
 for (var i = 8 ; i<19 ; i++){
-  btn[i] = $("#hour-"+i).children("button").click(function(){
+
+  btn = $("#hour-"+i).children("button").click(function(){
     alert("button " + this.btn[i] + " was clicked", position());
       function positon(){
         return this.btn[i];
@@ -44,25 +105,75 @@ for (var i = 8 ; i<19 ; i++){
   }) 
   console.log(btn[i]);
 }
+*/
 
-for (var i = 8 ; i<19 ; i++){
-  
+//Obtain button and store information from the text area to local storage
+$("#hour-8").children("button").click(function(){
+  alert("8 AM event saved");
+  var input8 = $("#hour-8").children("textarea").val();
+  console.log(input8);
+  localStorage.setItem("#hour-8", JSON.stringify(input8));
+})
+$("#hour-9").children("button").click(function(){
+  alert("9 AM event saved");
+  var input9 = $("#hour-9").children("textarea").val();
+  console.log(input9);
+  localStorage.setItem("#hour-9", JSON.stringify(input9));
+})
+$("#hour-10").children("button").click(function(){
+  alert("10 AM event saved");
+  var input10 = $("#hour-10").children("textarea").val();
+  console.log(input10);
+  localStorage.setItem("#hour-10", JSON.stringify(input10));
+})
+$("#hour-11").children("button").click(function(){
+  alert("11 AM event saved");
+  var input11 = $("#hour-11").children("textarea").val();
+  console.log(input11);
+  localStorage.setItem("#hour-11", JSON.stringify(input11));
+})
+$("#hour-12").children("button").click(function(){
+  alert("12 PM event saved");
+  var input12 = $("#hour-12").children("textarea").val();
+  console.log(input12);
+  localStorage.setItem("#hour-12", JSON.stringify(input12));
+})
+$("#hour-13").children("button").click(function(){
+  alert("1 PM event saved");
+  var input13 = $("#hour-13").children("textarea").val();
+  console.log(input13);
+  localStorage.setItem("#hour-13", JSON.stringify(input13));
+})
+$("#hour-14").children("button").click(function(){
+  alert("2 PM event saved");
+  var input14 = $("#hour-14").children("textarea").val();
+  console.log(input14);
+  localStorage.setItem("#hour-14", JSON.stringify(input14));
+})
+$("#hour-15").children("button").click(function(){
+  alert("3 PM event saved");
+  var input15 = $("#hour-15").children("textarea").val();
+  console.log(input15);
+  localStorage.setItem("#hour-15", JSON.stringify(input15));
+})
+$("#hour-16").children("button").click(function(){
+  alert("4 PM event saved");
+  var input16 = $("#hour-16").children("textarea").val();
+  console.log(input16);
+  localStorage.setItem("#hour-16", JSON.stringify(input16));
+})
+$("#hour-17").children("button").click(function(){
+  alert("5 PM event saved");
+  var input17 = $("#hour-17").children("textarea").val();
+  console.log(input17);
+  localStorage.setItem("#hour-17", JSON.stringify(input17));
+})
+$("#hour-18").children("button").click(function(){
+  alert("6 PM event saved");
+  var input18 = $("#hour-18").children("textarea").val();
+  console.log(input18);
+  localStorage.setItem("#hour-18", JSON.stringify(input18));
+})
 
-}
-
-
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
   });
   
